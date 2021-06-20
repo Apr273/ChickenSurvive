6307ac06499683374b3b3921acebdcef42c1d10e
strange361 <83707800+strange361@users.noreply.github.com>
2021-06-20 22:02:34 +0800

    Add files via upload
    map的resource资源

---
+++ Map/shoot.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="子弹" tilewidth="10" tileheight="10" tilecount="124" columns="4">
+ <image source="png文件/7.svg" width="49" height="318"/>
+</tileset>
---
+++ Map/tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="树木" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="《甲武天图》仙侠类游戏素材-书目(shumu)_爱给网_aigei_com.png" width="88" height="112"/>
+</tileset>
